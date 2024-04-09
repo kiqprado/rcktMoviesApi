3aff2fb3f56dd7cf31467144b15b056585110305
Kaique Prado <kiqprado@outlook.com>
2024-04-09 13:04:12 -0300

    feat: Knex Create Fix.

--- src/database/knex/migrations/20231026132934_createNotes.js
+++ src/database/knex/migrations/20231026132934_createNotes.js
@@ -1,13 +1,18 @@
-exports.up = knex =>
-  knex.schema.createTable('notes', table => {
-    table.increments('id')
-    table.text('title')
-    table.text('description')
-    table.integer("rate");
-    table.integer('user_id').references('id').inTable('users')
+exports.up = async knex => {
+  const hasNotesTable = await knex.schema.hasTable('notes')
 
-    table.timestamp('created_at').default(knex.fn.now())
-    table.timestamp('updated_at').default(knex.fn.now())
-  })
+  if (!hasNotesTable) {
+    knex.schema.createTable('notes', table => {
+      table.increments('id')
+      table.text('title')
+      table.text('description')
+      table.integer('rate')
+      table.integer('user_id').references('id').inTable('users')
+
+      table.timestamp('created_at').default(knex.fn.now())
+      table.timestamp('updated_at').default(knex.fn.now())
+    })
+  }
+}
 
 exports.down = knex => knex.schema.dropTable('notes')
--- src/database/knex/migrations/20231027135006_createTags.js
+++ src/database/knex/migrations/20231027135006_createTags.js
@@ -1,14 +1,19 @@
-exports.up = knex =>
-  knex.schema.createTable('tags', table => {
-    table.increments('id')
-    table.text('name').notNullable()
+exports.up = async knex => {
+  const hasTagsTable = await knex.schema.hasTable('tags')
 
-    table
-      .integer('note_id')
-      .references('id')
-      .inTable('notes')
-      .onDelete('CASCADE')
-    table.integer('user_id').references('id').inTable('users')
-  })
+  if (!hasTagsTable) {
+    knex.schema.createTable('tags', table => {
+      table.increments('id')
+      table.text('name').notNullable()
+
+      table
+        .integer('note_id')
+        .references('id')
+        .inTable('notes')
+        .onDelete('CASCADE')
+      table.integer('user_id').references('id').inTable('users')
+    })
+  }
+}
 
 exports.down = knex => knex.schema.dropTable('tags')
